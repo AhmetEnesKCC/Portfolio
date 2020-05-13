a421a3b16b63ca3334d6c2ecf484761c2425017b
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-05-14 01:15:49 +0300

    Updates

---
+++ precache-manifest.728fca0f7c746289d40fd80fed20ba2b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ab5c75d79928e42dc640ac0dd842eba",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5d625ff0793fe9cdc746",
+    "url": "/Portfolio/static/css/main.6c6afd5d.chunk.css"
+  },
+  {
+    "revision": "e908b23e8959d92fce6b",
+    "url": "/Portfolio/static/js/2.d896c53d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.d896c53d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d625ff0793fe9cdc746",
+    "url": "/Portfolio/static/js/main.558d09a6.chunk.js"
+  },
+  {
+    "revision": "4a50002865ce6bd649aa",
+    "url": "/Portfolio/static/js/runtime-main.d26897fe.js"
+  },
+  {
+    "revision": "a76d6431f95e3c012c91f0e99d2a208c",
+    "url": "/Portfolio/static/media/Asset 2.a76d6431.png"
+  }
+]);
