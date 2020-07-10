65ddaeacae05f232e7695d07bc2683930424f5e3
Ahmet Enes KCC <ahmetkcc2001@gmail.com>
2020-07-10 18:37:02 +0300

    fresh start to V2.0

--- src/gulpFile.js
+++ src/gulpFile.js
@@ -1,13 +1,11 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-sass.compiler = require("node-sass")
+sass.compiler = require("node-sass");
 
-gulp.task("sass",function() {
-    return gulp.src("./SCSS/**/*.scss")
-    .pipe(sass())
-    .pipe(gulp.dest("./CSS"))
-})
+gulp.task("sass", function () {
+  return gulp.src("./SCSS/**/*.scss").pipe(sass()).pipe(gulp.dest("./CSS"));
+});
 
-gulp.task("watch", function() {
-    gulp.watch("./SCSS/**/*.scss", gulp.series("sass"))
-})
+gulp.task("watch", function () {
+  gulp.watch("./SCSS/**/*.scss", gulp.series("sass"));
+});
